2490f39ccd097cc0144cc619703f9659ca10d81e
tootouch <jaehyuk_heo@korea.ac.kr>
2023-05-03 23:53:29 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 model_list="resnet18 resnet50 densenet121 densenet161"
-datasets="CIFAR10 CIFAR100"
+datasets="CIFAR10 CIFAR100 SVHN tiny_imagenet_200"
 
 for d in $datasets
 do
     for m in $model_list
     do
-        python main.py --yaml_config ./configs/$d/$m-finetune_set.yaml
-        python main.py --yaml_config ./configs/$d/$m-pretrained_set.yaml
+        python main.py --default_setting default_configs.yaml --dataname $d --modelname $m --normalize finetune
+        python main.py --default_setting default_configs.yaml --dataname $d --modelname $m --normalize pretrain
     done
 done
